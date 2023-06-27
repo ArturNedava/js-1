5ccc3e81bc702bc69388701e59aaf85d9152adf0
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-06-27 16:34:11 +0200

    First commit

---
+++ ladno.js
@@ -0,0 +1,44 @@
+
+let age = 12;
+
+console.log(age)
+
+let name = "Artur";
+
+console.log("Artur")
+
+let  isStudent = true;
+
+console.log( isStudent)
+
+let myString
+
+console.log("У нас есть что было,и нужно смотреть какой мы можем.Виталий Кличко")
+
+let myNumber = 10;
+
+console.log(myNumber+10);
+
+let myNull = null;
+
+console.log(null)
+
+alert('hello')
+
+let prm = prompt("Здравствуй друг")
+
+confirm("спасибо за подтверждение")
+
+console.log(prm);
+
+let conf = confirm("действие отменино")
+
+console.log(typeof conf);
+
+
+
+
+
+
+
+
